cebd74f68076af628bb0f2389230228a382bb3c1
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-01-09 19:28:17 +0100

    Add security

--- src/main/java/com/moldavets/finalproject/rest/LoginController.java
+++ src/main/java/com/moldavets/finalproject/rest/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AccountingRestController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String landingPage() {
-        return "index";
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
     }
 
 }
